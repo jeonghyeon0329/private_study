0c69ee45f695ba699bf599c6c95f2ac58137cd8d
Junghyeon0329 <sptlsrja@gmail.com>
2024-11-23 21:35:43 +0900

    layers 효율성

--- MLP.py
+++ MLP.py
@@ -21,14 +21,27 @@ mnist dataset:
 ##  이미지 데이터를 255로 나누어 0과 1 사이로 변환하는 것은 신경망 모델이 효과적으로 학습할 수 있도록 돕는 중요한 전처리 단계입니다.
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+# model = tf.keras.models.Sequential([
+#     tf.keras.layers.Flatten(input_shape = (28,28)), ## 2D 이미지를 1D 벡터로 변환
+#     tf.keras.layers.Dense(239, activation='relu'), ## 239개의 노드를 가진 완전 연결(Dense) 레이어
+#     tf.keras.layers.Dropout(0.2), ## 20%의 노드를 무작위로 제거하여 과적합(overfitting)을 방지
+#     tf.keras.layers.Dense(32, activation='relu'),  ## 32개의 노드를 가진 완전 연결(Dense) 레이어 (RELU 활성화 함수)
+#     tf.keras.layers.Dropout(0.2), ## 또 다른 20%의 노드를 무작위로 제거
+#     tf.keras.layers.Dense(10) ## 최종 레이어는 10개의 노드를 가진 완전 연결(Dense) 레이어입니다. 이 레이어는 MNIST 데이터셋의 10개의 클래스를 예측하기 위한 출력 레이어입니다. 
+# ])
+
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape = (28,28)), ## 2D 이미지를 1D 벡터로 변환
-    tf.keras.layers.Dense(239, activation='relu'), ## 239개의 노드를 가진 완전 연결(Dense) 레이어
-    tf.keras.layers.Dropout(0.2), ## 20%의 노드를 무작위로 제거하여 과적합(overfitting)을 방지
-    tf.keras.layers.Dense(32, activation='relu'),  ## 32개의 노드를 가진 완전 연결(Dense) 레이어 (RELU 활성화 함수)
-    tf.keras.layers.Dropout(0.2), ## 또 다른 20%의 노드를 무작위로 제거
-    tf.keras.layers.Dense(10) ## 최종 레이어는 10개의 노드를 가진 완전 연결(Dense) 레이어입니다. 이 레이어는 MNIST 데이터셋의 10개의 클래스를 예측하기 위한 출력 레이어입니다. 
+    tf.keras.layers.Rescaling(1./255, input_shape=(28, 28, 1)),  # 이미지 리스케일링
+    tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
 ])
+
 loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True)
 model.compile(optimizer = 'adam', loss = loss_func, metrics = ['accuracy'])
 model.fit(x_train, y_train, epochs = 10)
