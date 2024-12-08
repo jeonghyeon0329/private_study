4a188dcfcf2c5efcc39850c8b9287a9663f967f1
Junghyeon0329 <sptlsrja@gmail.com>
2024-12-08 18:01:21 +0900

    dot, matmul 차이

--- notepad.py
+++ notepad.py
@@ -11,4 +11,16 @@ a = np.array([1,2,3], dtype='int32') ## 1차원, 크기 : 3
 b = np.array([[9.0,8.0,7.0],[6.0,5.0,4.0]]) 
 
 a = tf.constant([1,2,3], dtype='int32') 
-print(a)
+
+v1 = np.array([1,2,3,4])
+v2 = np.array([5,6,7,8])
+
+print(np.vstack([v1,v2,v1,v2])) # 이어붙이기만 가능
+print(np.hstack([v1,v2,v1,v2])) # 이어붙이기만 가능
+print()
+print(tf.stack([v1,v2,v1,v2], axis = 0))
+# print(tf.concat([v1,v2,v1,v2], axis = 1))
+
+print(np.random.uniform(0, 1, (4,2)))
+print(tf.random.uniform((4,2), 0, 1))
+#4-1 
